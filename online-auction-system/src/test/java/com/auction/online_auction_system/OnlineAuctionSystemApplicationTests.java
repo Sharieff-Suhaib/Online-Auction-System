d3cfc443cfac35d7f3c435d38ad1da8f13cc58a1
Suhaib Sharieff <suhaibsharieff05@gmail.com>
2025-10-21 13:09:50 +0530

    initial spring setup

---
+++ online-auction-system/src/test/java/com/auction/online_auction_system/OnlineAuctionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auction.online_auction_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAuctionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
